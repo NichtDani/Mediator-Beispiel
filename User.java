70cb595e398eec4c2fbc1887b2660cedb3fbf5a9
NichtDani <37781460+NichtDani@users.noreply.github.com>
2024-01-25 21:44:35 +0100

    Add files via upload

--- User.java
+++ User.java
@@ -18,24 +18,29 @@ public class User implements IUser{
         this.mediator = mediator;
     }
 
+    @Override
     public void createGroup(String groupName) {
         mediator.createGroup(groupName);
     }
 
+    @Override
     public void joinGroup(String groupName) {
         mediator.addUserToGroup(groupName, this);
     }
 
+    @Override
     public void sendToGroup(String msg, String groupName) {
         System.out.println(this.name + ": Sending message to group [" + groupName + "] = " + msg);
         mediator.sendMessageToGroup(msg, groupName, this);
     }
 
+    @Override
     public void send(String msg) {
         System.out.println(this.name + ": Sending message = " + msg);
         mediator.sendMessage(msg, this);
     }
 
+    @Override
     public void receive(String msg) {
         System.out.println(this.name + ": Received message = " + msg);
     }
